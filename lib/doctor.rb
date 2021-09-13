2cc125de8ab872bf46f2e34e10fbce6a700c626f
Corban <sonnemanc@gmail.com>
2021-09-13 17:58:29 -0600

    commit tree c1bee579b0ecdbac7e012054bb1236928ac6c59d

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,39 @@
+class Doctor
 
+    attr_accessor   :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |visit|
+            visit.doctor == self
+          end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, self, patient)
+    end
+
+    def patients
+        appointments.map do |visit|
+            visit.patient
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,35 @@
 
+class Genre
+
+    attr_accessor   :name,  :songs
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = songs
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.genre == self
+            
+        end
+    end
+
+    def artists
+        songs.map do |song|
+            song.artist
+        end
+    end
+end
+
